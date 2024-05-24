a7f4ac589783e4bb67cd2b4ea590180c6a4393c0
ao627515 <abdo.ouedraogo.03@gmail.com>
2024-05-24 21:07:47 +0000

    changement de la nomenclature index en listeContact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function index(){
-        return view('contacts.index');
+    public function listeContact(){
+        return view('contacts.listeContact');
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('contact.index');
+    return redirect()->route('contact.listeContact');
 });
 
-Route::get('contacts', [ContactController::class, 'index'])->name('contact.index');
+Route::get('contacts', [ContactController::class, 'listeContact'])->name('contact.listeContact');
